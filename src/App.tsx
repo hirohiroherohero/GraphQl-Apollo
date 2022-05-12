fb194f83cb24c1a0993bdfcf0b6be45b23d3df59
hiro <hiro@onuii.com>
2022-05-12 16:48:35 +0900

    router설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import globalStyles from '@assets/css/globalStyle.css';
 import { Global } from '@emotion/react';
+import Detail from '@pages/Detail';
+import Home from '@pages/Home';
 import 'normalize.css';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <Global styles={globalStyles} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
     </>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
       // '@components': path.resolve(__dirname, 'src/components'),
       // '@pages': path.resolve(__dirname, 'src/pages'),
     },
